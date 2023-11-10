b3f44998e6b12aaaff64173da7b03079911260ce
MonojitWASS <monjitd@webappssoft.com>
2023-11-10 18:00:08 +0530

    Upto-Created middleware  for custom error handler

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 
 dotenv.config();
 
@@ -16,9 +17,23 @@ mongoose.connect(process.env.MONGO)
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
 });
 
-console.log(userRouter)
+
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal server error";
+
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
