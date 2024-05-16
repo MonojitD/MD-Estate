b28b298537abb48b7baa4c75c36ae6f5af81add4
MonojitD <monojitdeb66@gmail.com>
2024-05-16 19:15:21 +0530

    Firebase Authentication, Protected route added

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { signInStart, signInSuccess, signInFailure } from '../store/user/userSlice';
-// import OAuth from '../components/OAuth';
+import OAuth from '../components/OAuth';
+
 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -68,11 +69,11 @@ export default function SignIn() {
         >
           {data?.loading ? 'Loading...' : 'Sign In'}
         </button>
-        {/* <OAuth/> */}
+        <OAuth/>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Do not have an account?</p>
-        <Link to={'/sign-in'}>
+        <Link to={'/signup'}>
           <span className='text-blue-700'>Sign up</span>
         </Link>
       </div>
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-// import OAuth from '../components/OAuth';
+import OAuth from '../components/OAuth';
 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
@@ -79,7 +80,7 @@ export default function SignUp() {
         >
           {loading ? 'Loading...' : 'Sign Up'}
         </button>
-        {/* <OAuth/> */}
+        <OAuth/>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
