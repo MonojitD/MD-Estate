8dfb97770aee68a6264789b550661c839af7c995
MonojitD <monojitdeb66@gmail.com>
2024-05-25 13:04:38 +0530

    Add show listing

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -18,6 +18,8 @@ const Profile = () => {
     const [filePerc, setFilePerc] = useState(0)
     const [fileUploadError, setFileUploadError] = useState(false)
     const [formData, setFormData] = useState({})
+    const [showListingsError, setShowListingsError] = useState(false);
+    const [userListings, setUserListings] = useState([]);
     const { openModal, setOpenModal, modalData, setModalData } = useContext(ModalContext)
 
     const dispatch = useDispatch();
@@ -148,6 +150,22 @@ const Profile = () => {
         });
     }
 
+    const handleShowListings = async () => {
+        try {
+            setShowListingsError(false);
+            const res = await fetch(`/api/user/listings/${currentUser._id}`);
+            const data = await res.json();
+            if (data.success === false) {
+                setShowListingsError(true);
+                return;
+            }
+
+            setUserListings(data);
+        } catch (error) {
+            setShowListingsError(true);
+        }
+    };
+
 
 
   return (
@@ -180,6 +198,54 @@ const Profile = () => {
             <span onClick={openDeleteModal} className='text-red-700 cursor-pointer hover:opacity-60'>Delete account</span>
             <span onClick={openLogoutModal} className='text-red-700 cursor-pointer hover:opacity-60'>Log out</span>
         </div>
+
+        <button onClick={handleShowListings} className='text-green-700 w-full'>
+            Show Listings
+        </button>
+        <p className='text-red-700 mt-5'>
+            {showListingsError ? 'Error showing listings' : ''}
+        </p>
+
+        {userListings && userListings.length > 0 && (
+            <div className='flex flex-col gap-4'>
+            <h1 className='text-center mt-7 text-2xl font-semibold'>
+                Your Listings
+            </h1>
+            {userListings.map((listing) => (
+                <div
+                key={listing._id}
+                className='border rounded-lg p-3 flex justify-between items-center gap-4'
+                >
+                <Link to={`/listing/${listing._id}`}>
+                    <img
+                    src={listing.imageUrls[0]}
+                    alt='listing cover'
+                    className='h-16 w-16 object-contain'
+                    />
+                </Link>
+                <Link
+                    className='text-slate-700 font-semibold  hover:underline truncate flex-1'
+                    to={`/listing/${listing._id}`}
+                >
+                    <p>{listing.name}</p>
+                </Link>
+
+                <div className='flex flex-col item-center'>
+                    <button
+                    onClick={() => handleListingDelete(listing._id)}
+                    className='text-red-700 uppercase'
+                    >
+                    Delete
+                    </button>
+                    <Link to={`/update-listing/${listing._id}`}>
+                    <button className='text-green-700 uppercase'>Edit</button>
+                    </Link>
+                </div>
+                </div>
+            ))}
+            </div>
+        )}
+
         <ToastContainer/>
         {openModal && <AlertBox />}
     </div>
